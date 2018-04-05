99cc0cf8d2aaab2196765e7a56d36ababdad2043
lshong98 <lshong9899@gmail.com>
2018-04-06 00:21:05 +0800

    add show next block feature

--- js/render.js
+++ js/render.js
@@ -3,42 +3,76 @@ Name: Lei Sing Hong
 Date: 22 March 2018
 Description: This js file is use to draw a game board.
 ***********************************/
-var canvas = document.getElementsByTagName( 'canvas' )[ 0 ]; //connect with html canvas tag
-var ctx = canvas.getContext( '2d' );
+var canvas = document.getElementById('mainscreen');
+var nextBlock = document.getElementById('nextBlock');
+//connect with html canvas tag
+var ctx = canvas.getContext('2d');
+var ntx = nextBlock.getContext('2d');
 var W = 300, H = 600; //game board weight and height
+var nW = 160, nH = 320; //nextBlock width and height
 var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
+var nBLOCK_W = nW / COLS, nBLOCK_H = nH / ROWS; //4, 4
 
 // draw a single square at (x, y)
-function drawBlock( x, y ) {
-    ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
-    ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
+function drawBlock(x, y) {
+    'use strict';
+    ctx.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
+    ctx.strokeRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1, BLOCK_H - 1);
+}
+
+function ndrawBlock(x, y) {
+    'use strict';
+    ntx.fillRect((nBLOCK_W * x) + 15, (nBLOCK_H * y) + 20, nBLOCK_W - 1, nBLOCK_H - 1);
+    ntx.strokeRect((nBLOCK_W * x) + 15, (nBLOCK_H * y) + 20, nBLOCK_W - 1, nBLOCK_H - 1);
 }
 
 // draws the board and the moving shape
 function render() {
-    ctx.clearRect( 0, 0, W, H );
+    'use strict';
+    var x, y;
+    ctx.clearRect(0, 0, W, H);
 
     ctx.strokeStyle = 'black';
-    for ( var x = 0; x < COLS; ++x ) {
-        for ( var y = 0; y < ROWS; ++y ) {
-            if ( board[ y ][ x ] ) {
-                ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];
-                drawBlock( x, y );
+    for (x = 0; x < COLS; x += 1) {
+        for (y = 0; y < ROWS; y += 1) {
+            if (board[y][x]) {
+                ctx.fillStyle = colors[board[y][x] - 1];
+                drawBlock(x, y);
             }
         }
     }
 
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'black'; //block color
-    for ( var y = 0; y < 4; ++y ) {
-        for ( var x = 0; x < 4; ++x ) {
-            if ( current[ y ][ x ] ) {
-                ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];
-                drawBlock( currentX + x, currentY + y );
+    for (y = 0; y < 4; y += 1) {
+        for (x = 0; x < 4; x += 1) {
+            if (current[y][x]) {
+                ctx.fillStyle = colors[current[y][x] - 1];
+                drawBlock(currentX + x, currentY + y);
+            }
+        }
+    }
+}
+
+function nrender() {
+    'use strict';
+    var x, y;
+    //remove previous moving
+    ntx.clearRect(0, 0, nW, nH);
+
+    ntx.fillStyle = 'red';
+    ntx.strokeStyle = 'black'; //block color
+    
+    for (y = 0; y < 4; y += 1) {
+        for (x = 0; x < 4; x += 1) {
+            if (next[y][x]) {
+                ntx.fillStyle = colors[next[y][x] - 1];
+                ndrawBlock(x, y);
             }
         }
     }
 }
 
 //repeat the execution function in 30 milliseconds
-setInterval( render, 30 );
+setInterval(render, 30);
+setInterval(nrender, 30);
--- js/tetris.js
+++ js/tetris.js
@@ -5,11 +5,12 @@ Description: This file is the code make the program run.
 ***********************************/
 
 //variable
-var score = 0;
-document.getElementById('score').innerHTML = score;
+//var score = 0;
+
 var bgMusic;
 var clSoundEfct;
 var score = 0;
+document.getElementById('score').innerHTML = score;
 var COLS = 10, ROWS = 20;
 var board = [];
 var lose;
@@ -17,21 +18,16 @@ var interval;
 var current; // current moving shape
 var currentX, currentY; // position of current shape
 var recordID; //variable to record shape
+var next;
 //create the shape such as square 
 var shapes = [
-    [ 1, 1, 1, 1 ],
-    [ 1, 1, 1, 0,
-      1 ],
-    [ 1, 1, 1, 0,
-      0, 0, 1 ],
-    [ 1, 1, 0, 0,
-      1, 1 ],
-    [ 1, 1, 0, 0,
-      0, 1, 1 ],
-    [ 0, 1, 1, 0,
-      1, 1 ],
-    [ 0, 1, 0, 0,
-      1, 1, 1 ]
+    [1, 1, 1, 1],
+    [1, 1, 1, 0, 1],
+    [1, 1, 1, 0, 0, 0, 1 ],
+    [1, 1, 0, 0, 1, 1 ],
+    [1, 1, 0, 0, 0, 1, 1 ],
+    [0, 1, 1, 0, 1, 1 ],
+    [0, 1, 0, 0, 1, 1, 1 ]
 ];
 var colors = [
     'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
@@ -39,71 +35,181 @@ var colors = [
 
 // creates a new 4x4 shape in global variable 'current'
 // 4x4 so as to cover the size when the shape is rotated
-function newShape() {
-    var id = Math.floor( Math.random() * shapes.length );
+function nextShape() {
+    'use strict';
+    var id = Math.floor(Math.random() * shapes.length), shape, y, x, i;
 	//while the previous shape is same with new shape, keep random in the loop until its not same
-	while(recordID == id){ 
-		id = Math.floor( Math.random() * shapes.length);
+	while (recordID === id) {
+		id = Math.floor(Math.random() * shapes.length);
 	}
 	recordID = id; //to assign new shape id into recordID
-    var shape = shapes[ id ]; // maintain id for shape chosen
+    shape = shapes[id]; // maintain id for shape chosen
 
-    current = [];
-    for ( var y = 0; y < 4; ++y ) {
-        current[ y ] = [];
-        for ( var x = 0; x < 4; ++x ) {
-            var i = 4 * y + x;
-            if ( typeof shape[ i ] != 'undefined' && shape[ i ] ) {
-                current[ y ][ x ] = id + 1;
-            }
-            else {
-                current[ y ][ x ] = 0;
+    next = [];
+    for (y = 0; y < 4; y += 1) {
+        next[y] = [];
+        for (x = 0; x < 4; x += 1) {
+            i = 4 * y + x;
+            if (typeof shape[i] !== 'undefined' && shape[i]) {
+                next[y][x] = id + 1;
+            } else {
+                next[y][x] = 0;
             }
         }
     }
-    // position where the shape will evolve
-    currentX = 5;
-    currentY = -1;
+    return recordID;
 }
 
 // clears the board
 function init() {
-    for ( var y = 0; y < ROWS; ++y ) {
-        board[ y ] = [];
-        for ( var x = 0; x < COLS; ++x ) {
-            board[ y ][ x ] = 0;
+    'use strict';
+    var x, y;
+    for (y = 0; y < ROWS; y += 1) {
+        board[y] = [];
+        for (x = 0; x < COLS; x += 1) {
+            board[y][x] = 0;
+        }
+    }
+}
+
+// checks if the resulting position of current shape will be feasible
+function valid(offsetX, offsetY, newCurrent) {
+    'use strict';
+    var x, y;
+    offsetX = offsetX || 0;
+    offsetY = offsetY || 0;
+    offsetX = currentX + offsetX;
+    offsetY = currentY + offsetY;
+    newCurrent = newCurrent || current;
+
+
+
+    for (y = 0; y < 4; y += 1) {
+        for (x = 0; x < 4; x += 1) {
+            if (newCurrent[y][x]) {
+                if (typeof board[y + offsetY] === 'undefined' || typeof board[y + offsetY][x + offsetX] === 'undefined' || board[y + offsetY][x + offsetX] || x + offsetX < 0 || y + offsetY >= ROWS || x + offsetX >= COLS) {
+                    if (offsetY === 1) {lose = true; } // lose if the current shape at the top row when checked
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+function newShape(initialID) {
+    'use strict';
+    var myNextID = initialID || $.cookie("cookieShape"), x, y, i, nextNextID = nextShape(), id = myNextID, shape = shapes[id]; // maintain id for color filling
+
+    $.cookie("cookieShape", nextNextID);
+    
+    current = [];
+    for (y = 0; y < 4; y += 1) {
+        current[y] = [];
+        for (x = 0; x < 4; x += 1) {
+            i = 4 * y + x;
+            if (typeof shape[i] !== 'undefined' && shape[i]) {
+                current[y][x] = id + 1;
+            } else {
+                current[y][x] = 0;
+            }
+        }
+    }
+    //console.log(current);
+    // position where the shape will evolve
+    currentX = 5;
+    currentY = 0;
+}
+
+//function to create button when it is clicked the game will start
+function createHomeStartButton() {
+	'use strict';
+	// 1. Create the button
+	var button = document.createElement("button"), body;
+	button.innerHTML = "Start Game";
+
+	// 2. Append somewhere
+	body = document.getElementsByTagName("body")[0];
+	body.appendChild(button);
+
+	// 3. Add event handler
+	button.addEventListener("click", function () {
+	    window.alert("Enjoy The Game");
+	    newGame();
+	    body.removeChild(button);
+	});
+}
+function homeScreen() {
+    'use strict';
+	createHomeStartButton();
+}
+
+// stop shape at its position and fix it to board
+function freeze() {
+    'use strict';
+    var x, y;
+    for (y = 0; y < 4; y += 1) {
+        for (x = 0; x < 4; x += 1) {
+            if (current[y][x]) {
+                board[y + currentY][x + currentX] = current[y][x];
+            }
+        }
+    }
+}
+
+// check if any lines are filled and clear them
+function clearLines() {
+    'use strict';
+    var x, y, rowFilled, yy;
+    for (y = ROWS - 1; y >= 0; y -= 1) {
+        rowFilled = true;
+        for (x = 0; x < COLS; x += 1) {
+            if (board[y][x] === 0) {
+                rowFilled = false;
+                break;
+            }
+        }
+        if (rowFilled) {
+            clSoundEfct.play();
+            //document.getElementById( 'clearsound' ).play();
+			score += 20;
+			document.getElementById('score').innerHTML = score;
+            for (yy = y; yy > 0; yy -= 1) {
+                for (x = 0; x < COLS; x += 1) {
+                    board[yy][x] = board[yy - 1][x];
+                }
+            }
+            y += 1;
         }
     }
 }
 
 // keep the element moving down, creating new shapes and clearing lines
 function tick() {
-    if ( valid( 0, 1 ) ) {
-        ++currentY;
-    }
-	//reach maximum height, end game
-	else if(currentY == -1)
-	{
-		if(confirm('Game Over!\nDo you want to play again?')){
+    'use strict';
+    if (valid(0, 1)) {
+        currentY += 1;
+    } else if (currentY === -1) {
+        //reach maximum height, end game
+		if (window.confirm('Game Over!\nDo you want to play again?')) {
 			score = 0;
 			document.getElementById('score').innerHTML = score;
-				newGame();
-			} else{
-				clearInterval(interval);
-				init();
-				homeScreen();
-			}
-    }
-    // if the element settled
-    else {
+            newGame();
+        } else {
+            clearInterval(interval);
+            init();
+            homeScreen();
+        }
+    } else {
+        // if the element settled
         freeze();
         clearLines();
         if (lose) {
-			if(confirm('Game Over!\nDo you want to play again?')){
+			if (window.confirm('Game Over!\nDo you want to play again?')) {
 				score = 0;
 				document.getElementById('score').innerHTML = score;
 				newGame();
-			} else{
+			} else {
 				clearInterval(interval);
 				init();
 				homeScreen();
@@ -114,198 +220,123 @@ function tick() {
     }
 }
 
-// stop shape at its position and fix it to board
-function freeze() {
-    for ( var y = 0; y < 4; ++y ) {
-        for ( var x = 0; x < 4; ++x ) {
-            if ( current[ y ][ x ] ) {
-                board[ y + currentY ][ x + currentX ] = current[ y ][ x ];
-            }
-        }
-    }
+//Start a new game when game over
+function newGame() {
+    'use strict';
+    var nextShapeId;
+    clearInterval(interval);
+    init();
+    nextShapeId = nextShape();
+    newShape(nextShapeId);
+    lose = false;
+    interval = setInterval(tick, 250);
 }
 
 // returns rotates the rotated shape 'current' perpendicularly anticlockwise
-function rotate( current ) {
-    var newCurrent = [];
-    for ( var y = 0; y < 4; ++y ) {
-        newCurrent[ y ] = [];
-        for ( var x = 0; x < 4; ++x ) {
-            newCurrent[ y ][ x ] = current[ 3 - x ][ y ];
+function rotate(current) {
+    'use strict';
+    var newCurrent = [], x, y;
+    for (y = 0; y < 4; y += 1) {
+        newCurrent[y] = [];
+        for (x = 0; x < 4; x += 1) {
+            newCurrent[y][x] = current[3 - x][y];
         }
     }
 
     return newCurrent;
 }
 
-// check if any lines are filled and clear them
-function clearLines() {
-    for ( var y = ROWS - 1; y >= 0; --y ) {
-        var rowFilled = true;
-        for ( var x = 0; x < COLS; ++x ) {
-            if ( board[ y ][ x ] == 0 ) {
-                rowFilled = false;
-                break;
-            }
-        }
-        if ( rowFilled ) {
-            clSoundEfct.play();
-            //document.getElementById( 'clearsound' ).play();
-			score += 20;
-			document.getElementById('score').innerHTML = score;
-            for ( var yy = y; yy > 0; --yy ) {
-                for ( var x = 0; x < COLS; ++x ) {
-                    board[ yy ][ x ] = board[ yy - 1 ][ x ];
-                }
-            }
-            ++y;
-        }
-    }
-}
 //press the key to move, rotate, store block or hard drop
-function keyPress( key ) {
-    switch ( key ) {
-        case 'left':
-            if ( valid( -1 ) ) {
-                --currentX;
-            }
-            break;
-        case 'right':
-            if ( valid( 1 ) ) {
-                ++currentX;
-            }
-            break;
-        case 'down':
-            if ( valid( 0, 1 ) ) {
-                ++currentY;
-            }
-            break;
-		case 'space':
-				while (valid( 0, 1 ))
-				{
-					++currentY;
-				}
-            break;
-        case 'rotate':
-            var rotated = rotate( current );
-            if ( valid( 0, 0, rotated ) ) {
-                current = rotated;
-            }
-            break;
-    }
-}
-
-// checks if the resulting position of current shape will be feasible
-function valid( offsetX, offsetY, newCurrent ) {
-    offsetX = offsetX || 0;
-    offsetY = offsetY || 0;
-    offsetX = currentX + offsetX;
-    offsetY = currentY + offsetY;
-    newCurrent = newCurrent || current;
-
-
-
-    for ( var y = 0; y < 4; ++y ) {
-        for ( var x = 0; x < 4; ++x ) {
-            if ( newCurrent[ y ][ x ] ) {
-                if ( typeof board[ y + offsetY ] == 'undefined'
-                  || typeof board[ y + offsetY ][ x + offsetX ] == 'undefined'
-                  || board[ y + offsetY ][ x + offsetX ]
-                  || x + offsetX < 0
-                  || y + offsetY >= ROWS
-                  || x + offsetX >= COLS ) {
-                    if (offsetY == 1) lose = true; // lose if the current shape at the top row when checked
-                    return false;
-                }
-            }
+function keyPress(key) {
+    'use strict';
+    switch (key) {
+    case 'left':
+        if (valid(-1)) {
+            currentX -= 1;
         }
+        break;
+    case 'right':
+        if (valid(1)) {
+            currentX += 1;
+        }
+        break;
+    case 'down':
+        if (valid(0, 1)) {
+            currentY += 1;
+        }
+        break;
+    case 'space':
+        while (valid(0, 1)) {
+            currentY += 1;
+        }
+        break;
+    case 'rotate':
+        var rotated = rotate(current);
+        if (valid(0, 0, rotated)) {
+            current = rotated;
+        }
+        break;
     }
-    return true;
-}
-//Start a new game when game over
-function newGame() {
-    clearInterval(interval);
-    init();
-    newShape();
-    lose = false;
-    interval = setInterval( tick, 250 );
 }
 
-function sound(src){
+
+
+function Sound(src) {
+    'use strict';
     this.sound = document.createElement("audio");
     this.sound.src = src;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.setAttribute("class", "bgMusic");
-    if(src ==="sound/backgroundMusic.mp3")
-        this.sound.setAttribute("class","bgMusic");
-    else if(src==="sound/clearLine.mp3")
-        this.sound.setAttribute("class","clearSound");
+    if (src === "sound/backgroundMusic.mp3") {
+        this.sound.setAttribute("class", "bgMusic");
+    } else if (src === "sound/clearLine.mp3") {
+        this.sound.setAttribute("class", "clearSound");
+    }
     this.sound.style.display = "none";
     document.body.appendChild(this.sound);
-    this.play = function(){
+    this.play = function () {
         this.sound.play();
     };
-    this.muted = function(){
+    this.muted = function () {
         this.sound.muted = true;
     };
-    this.resume = function(){
+    this.resume = function () {
         this.sound.muted = false;
-    }
+    };
 }
 
-function playingMusic(){
-    if (document.getElementsByClassName('bgMusic').length == 0) {
-        bgMusic = new sound("sound/backgroundMusic.mp3");
+function playingMusic() {
+    'use strict';
+    if (document.getElementsByClassName('bgMusic').length === 0) {
+        bgMusic = new Sound("sound/backgroundMusic.mp3");
     }
     bgMusic.play();
 }
 
-function clearLineSound(){
-    if(document.getElementsByClassName("bgMusic").length === 0){
-        clSoundEfct = new sound("sound/clearLine.mp3");
+function clearLineSound() {
+    'use strict';
+    if (document.getElementsByClassName("bgMusic").length === 0) {
+        clSoundEfct = new Sound("sound/clearLine.mp3");
     }
 }
 
-setInterval(function(){playingMusic();}, 1000);
-
-
-//function to create button when it is clicked the game will start
-function createHomeStartButton(){
-	
-	// 1. Create the button
-	var button = document.createElement("button");
-	button.innerHTML = "Start Game";
-
-	// 2. Append somewhere
-	var body = document.getElementsByTagName("body")[0];
-	body.appendChild(button);
-
-	// 3. Add event handler
-	button.addEventListener ("click", function() {
-	alert("Enjoy The Game");
-	newGame();
-	body.removeChild(button);
-	});
-}
-function homeScreen(){
-	createHomeStartButton();
-}
+setInterval(function () {'use strict'; playingMusic(); }, 1000);
 
 homeScreen();
-$(document).ready(function(){
+$(document).ready(function () {
+    'use strict';
     clearLineSound();
     var flag = true;
-    $('#tMusic').click(function(){
+    $('#tMusic').click(function () {
         if (flag === true) {
             bgMusic.muted();
             clSoundEfct.muted();
             flag = false;
-        }
-        else {
+        } else {
             bgMusic.resume();
             clSoundEfct.resume();
             flag = true;
-        }   
+        }  
     });
 });
